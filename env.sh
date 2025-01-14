235dabde26ddc56e3428a23ff72d9ca07e030227
Craig Nelson <34287157+supahcraig@users.noreply.github.com>
2025-01-14 14:54:40 -0600

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,13 @@
+echo 'preparing the environment...'
+
+chmod 777 ./pgadmin-data
+
+alias up='docker compose up --build'
+alias down='docker compose down --volumes'
+alias debup='docker compose up connect --build'
+alias debdown='docker compose down connect'
+alias oraup='docker compose up oracle-db-source'
+alias oradown='docker compose down oracle-db-source'
+
+
+echo 'done!'
