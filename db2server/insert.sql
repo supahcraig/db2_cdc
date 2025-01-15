5b048462339631cbc9d69d65455ad2fe78dc4cfb
Craig Nelson <34287157+supahcraig@users.noreply.github.com>
2025-01-15 10:49:04 -0600

    Create insert.sql

---
+++ db2server/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO DB2INST1.CUSTOMERS (FIRST_name, last_name, email) VALUES ('Robert', 'Plant', 'feather@zep.com');
+INSERT INTO DB2INST1.CUSTOMERS (FIRST_name, last_name, email) VALUES ('Jimmy', 'Page', 'zoso@zep.com');
+INSERT INTO DB2INST1.CUSTOMERS (FIRST_name, last_name, email) VALUES ('John Paul', 'Jones', 'celtic@zep.com');
+INSERT INTO DB2INST1.CUSTOMERS (FIRST_name, last_name, email) VALUES ('John', 'Bonham', 'circles@zep.com');
